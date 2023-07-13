daab662d92275c41b82e2112ebdb1bcd571e4d16
ratbek1 <115623152+ratbek1@users.noreply.github.com>
2023-07-13 10:06:02 +0600

    startActions (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import { Route, Routes } from 'react-router-dom';
+import Admin from './components/Admin';
+import Orders from './components/Orders';
+import Menu from './components/Menu';
 
 function App() {
   return (
     <div className="App">
-
+      <Header/>
+      <Routes>
+        <Route path='/admin' element={ <Admin/> }/>
+        <Route path='/orders' element={ <Orders/> }/>
+        <Route path='/menu' element={ <Menu/> }/>
+      </Routes>
     </div>
   );
 }
